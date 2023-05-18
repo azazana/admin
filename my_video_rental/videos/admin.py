97b0c1757108308525a1a3e6e29bc9bb762a9977
azazana <azazana@list.ru>
2023-05-18 22:48:58 +0300

    list_desplay

--- my_video_rental/videos/admin.py
+++ my_video_rental/videos/admin.py
@@ -5,6 +5,11 @@ class MovieAdmin(admin.ModelAdmin):
 
     fields = ['release_year','title','length']
 
+    search_fields = ['title']
+
+    list_filter = ['release_year','length']
+    list_display = ['title','release_year','length']
+
 admin.site.register(models.Custemer)
 admin.site.register(models.Movie,MovieAdmin)
 
